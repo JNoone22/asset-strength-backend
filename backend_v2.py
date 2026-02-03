d2cf82ea5d908b01e72805a7ce465ee8ec00e319
JNoone22 <161664680+JNoone22@users.noreply.github.com>
2026-02-03 00:09:06 -0500

    Update backend_v2.py

--- backend_v2.py
+++ backend_v2.py
@@ -13,7 +13,7 @@ import time
 import pytz
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": ["https://sage-biscotti-6e007c.netlify.app", "http://localhost:8000"]}})
 
 # Configuration
 TWELVE_DATA_API_KEY = os.getenv('TWELVE_DATA_API_KEY', 'YOUR_API_KEY_HERE')
